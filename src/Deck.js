9ae5e7980ec3a836d7c756c2a63bc017837d922c
Molly Arhammar <mollyarhammar@gmail.com>
2016-10-02 22:09:46 +0100

    starts vaguely on the Deck type

---
+++ src/Deck.js
@@ -0,0 +1,56 @@
+/**
+ * Module for Deck.
+ *
+ * @author Molly Arhammar
+ * @version 1.0.0
+ */
+const Card = require('../src/Card.js');
+
+class Deck {
+    constructor() {
+        let unused = [];
+        let used = [];
+
+        let suits = Card.getSuits();
+        let values = Card.getValues();
+
+        suits.forEach(function(suit) {
+            values.forEach(function (value) {
+                unused.push(new Card(suit, value));
+            });
+        });
+
+        Object.defineProperties(this, {
+            unusedCards: {
+                get: ,
+                set: ,
+                enumerable: ,
+                configurable:
+            },
+            usedCards: {
+                get: ,
+                set: ,
+                enumerable: ,
+                configurable:
+            }
+
+        });
+    }
+
+    toString() {
+        output = '';
+
+    }
+
+    shuffle() {
+
+    }
+
+    deal(amount) {
+
+    }
+
+    giveBackToDeck(cards[]) {
+
+    }
+}
