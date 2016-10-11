16596ad94dd7c3d389a4ff39e3afde7b929c92cb
Molly Arhammar <mollyarhammar@gmail.com>
2016-10-11 18:58:27 +0100

    implements some property testing

--- test/Card.test.js
+++ test/Card.test.js
@@ -10,8 +10,8 @@
 const expect = require('chai').expect;
 
 describe('Card', () => {
-    const SUIT = 'clubs';
-    const VALUE = 'ten';
+    const SUIT = 'CLUBS';
+    const VALUE = 'TEN';
 
     let Card;
 
@@ -26,7 +26,7 @@ describe('Card', () => {
         let aCard;
 
         beforeEach(() => {
-            // Create a new ToDoItem before every test.
+            // Create a new Card before every test.
             aCard = new Card(SUIT, VALUE);
         });
 
@@ -45,6 +45,66 @@ describe('Card', () => {
             done();
         });
 
+    });
+
+    describe('Properties', () => {
+        describe('suit', () => {
+            let aCard;
+
+            beforeEach(() => {
+                // Create a new Card before every test.
+                aCard = new Card(SUIT, VALUE);
+            });
+
+            it('should return the suit', (done) => {
+                expect(aCard.suit).to.equal(SUIT);
+                done();
+            });
+
+            it('should throw an Error if the suit tries to be changed', (done) => {
+                expect(() => {
+                    aCard.suit = 'spades';
+                }).to.throw(Error);
+                done();
+            });
+
+            it('should throw a TypeError if the suit is initially set to an invalid value', (done) => {
+                expect(() => {
+                    new Card('hej', VALUE);
+                }).to.throw(Error);
+                done();
+            });
+
+        });
+
+        describe('value', () => {
+            let aCard;
+
+            beforeEach(() => {
+                // Create a new Card before every test.
+                aCard = new Card(SUIT, VALUE);
+            });
+
+            it('should return the value', (done) => {
+                expect(aCard.value).to.equal(VALUE);
+                done();
+            });
+
+            it('should throw an Error if the value tries to be changed', (done) => {
+                expect(() => {
+                    aCard.value = 'five';
+                }).to.throw(Error);
+                done();
+            });
+
+            it('should throw a TypeError if the value is initially set to an invalid value', (done) => {
+                expect(() => {
+                    new Card(SUIT, 'hej');
+                }).to.throw(Error);
+                done();
+            });
+
+        });
     });
 
 });
