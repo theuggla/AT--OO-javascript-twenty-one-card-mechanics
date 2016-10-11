13eeea1e5387110ca23e61e0aaa8aa89c8dee69e
Molly Arhammar <mollyarhammar@gmail.com>
2016-10-11 18:40:22 +0100

    actually implements some constructor testing

--- test/Card.test.js
+++ test/Card.test.js
@@ -1,1 +1,50 @@
+/**
+ * Tests for the Card type.
+ *
+ * @author Molly Arhammar
+ * @version 1.16.1
+ */
 
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('Card', () => {
+    const SUIT = 'clubs';
+    const VALUE = 'ten';
+
+    let Card;
+
+    describe('Type', () => {
+        it('should be defined', (done) => {
+            Card = require('../src/Card');
+            done();
+        });
+    });
+
+    describe('Constructor', () => {
+        let aCard;
+
+        beforeEach(() => {
+            // Create a new ToDoItem before every test.
+            aCard = new Card(SUIT, VALUE);
+        });
+
+        it('should be instance of Card', (done) => {
+            expect(aCard).to.be.an.instanceOf(Card);
+            done();
+        });
+
+        it('should have property suit', (done) => {
+            expect(aCard).to.have.property('suit');
+            done();
+        });
+
+        it('should have property value', (done) => {
+            expect(aCard).to.have.property('value');
+            done();
+        });
+
+    });
+
+});
