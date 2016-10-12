f012089c2aa2f481f08a9dc9442bc3301b186b91
Molly Arhammar <mollyarhammar@gmail.com>
2016-10-12 15:03:35 +0100

    adds static members tests

--- test/Card.test.js
+++ test/Card.test.js
@@ -239,19 +239,19 @@ describe('Card', () => {
                 done();
             });
 
-            it('should return 0', (done) => {
+            it('should return equal', (done) => {
                 let anotherCard = new Card(SUIT, VALUE);
                 expect(aCard.compareTo(anotherCard)).to.equal(0);
                 done();
             });
 
-            it('should return -3', (done) => {
+            it('should return negative', (done) => {
                 let anotherCard = new Card('HEARTS', 'KING');
                 expect(aCard.compareTo(anotherCard)).to.equal(-3);
                 done();
             });
 
-            it('should return 8', (done) => {
+            it('should return positive', (done) => {
                 let anotherCard = new Card('HEARTS', 'TWO');
                 expect(aCard.compareTo(anotherCard)).to.equal(8);
                 done();
@@ -272,7 +272,7 @@ describe('Card', () => {
                 done();
             });
 
-            it('should return 10', (done) => {
+            it('should return correct value', (done) => {
                 expect(aCard.valueOf()).to.equal(10);
                 done();
             });
@@ -286,4 +286,91 @@ describe('Card', () => {
         });
     });
 
+    describe('Static properties', () => {
+        describe('suits', () => {
+            it('should be defined', (done) => {
+                expect(Card).to.have.property('suits');
+                done();
+            });
+
+            it('should return an Array', (done) => {
+                expect(Card.suits).to.be.an.instanceof(Array);
+                done();
+            });
+
+            it('should return a copy', (done) => {
+                let suits = Card.suits;
+                suits = ['HEJ', 'DÅ'];
+                expect(Card.suits).to.not.equal(suits);
+                done();
+            });
+
+            it('should throw an Error if the isOverdue is set (should be read-only!)', (done) => {
+                expect(() => {
+                    Card.suits = ['HEJ', 'DÅ'];
+                }).to.throw(Error);
+                done();
+            });
+        });
+
+        describe('values', () => {
+            it('should be defined', (done) => {
+                expect(Card).to.have.property('values');
+                done();
+            });
+
+            it('should return an Array', (done) => {
+                expect(Card.values).to.be.an.instanceof(Array);
+                done();
+            });
+
+            it('should return a copy', (done) => {
+                let values = Card.values;
+                values = ['HEJ', 'DÅ'];
+                expect(Card.values).to.not.equal(values);
+                done();
+            });
+
+            it('should throw an Error if the isOverdue is set (should be read-only!)', (done) => {
+                expect(() => {
+                    Card.values = ['HEJ', 'DÅ'];
+                }).to.throw(Error);
+                done();
+            });
+        });
+
+    });
+
+    describe('Static methods', () => {
+        describe('isValid method', () => {
+            it('should be defined', (done) => {
+                expect(Card).to.have.property('isValid').that.is.a('Function');
+                done();
+            });
+
+            it('should return true', (done) => {
+                let aCard = new Card(SUIT, VALUE);
+                expect(Card.isValid(aCard)).to.equal(true);
+                done();
+            });
+
+            it('should return false when it doesn\'t receive a Card', (done) => {
+                expect(Card.isValid(undefined)).to.equal(false);
+                expect(Card.isValid(null)).to.equal(false);
+                expect(Card.isValid([])).to.equal(false);
+                expect(Card.isValid(42)).to.equal(false);
+                expect(Card.isValid('Hej')).to.equal(false);
+                expect(Card.isValid(true)).to.equal(false);
+                expect(Card.isValid({suit: 'HEARTS', value: 'TEN'})).to.equal(false);
+                done();
+            });
+
+            it('should throw an Error if tries to be reassigned', (done) => {
+                expect(() => {
+                    Card.isValid = ['HEJ', 'DÅ'];
+                }).to.throw(Error);
+                done();
+            });
+        });
+    });
 });
