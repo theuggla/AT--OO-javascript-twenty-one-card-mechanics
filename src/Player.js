4c2409e06b63f3549eb115ec8e23ee259fd4f1d1
Molly Arhammar <mollyarhammar@gmail.com>
2016-10-12 20:00:53 +0100

    implements the Player outline

--- src/Player.js
+++ src/Player.js
@@ -7,56 +7,112 @@
 
 'use strict';
 
-function Player(name = 'aPlayer') {
+const Card = require('../src/Card.js');
+
+function Player(name) {
+
     let theName;
-    let hand = [];
+    let theHand = [];
 
     Object.defineProperties(this, {
         name: {
-            get: getName,
-            set: setName,
+            get: function() {
+                return theName;
+            },
+            set: function(name) {
+                if (name.length > 0 && name.length < 20) {
+                    theName = name;
+                }
+            },
             enumerable: true,
-            configurable: true
+            configurable: false
         },
         hand: {
-            get: setHand,
-            set: getHand,
+            get: function() {
+                return copyHand(theHand);
+            },
+            set: function(hand) {
+                if (Array.isArray(hand)) {
+                    if (hand.length === 0) { //allow to be set to 0
+                        theHand = [];
+                    } else if (isValidHand) {
+                        theHand = copyHand(hand);
+                    } else {
+                        throw new Error('The hand is not a valid hand.');
+                    }
+                } else {
+                    throw new TypeError('Must be Array!');
+                }
+            },
             enumerable: true,
-            configurable: true
+            configurable: false
         },
         inPlay: {
             get: function() {
-
+                return this.valueOf() < 21;
             },
             enumerable: false,
-            configurable: true
+            configurable: false
         }
     });
 
-    function getHand() {
+    this.name = name;
 
+    //helper functions
+    function copyHand(hand) {
+        let newHand = [];
+        hand.forEach(function(card) {
+            newHand.push(card.clone());
+        });
+        return newHand;
     }
-    function setHand() {
 
-    }
-    function getName() {
-
-    }
-    function setName() {
-
+    function isValidHand(hand) {
+        hand.forEach(function(card) {
+            if (!Card.isValid(card)) {
+                return false;
+            }
+        });
+        return true;
     }
 }
 
 Player.prototype.valueOf = function() {
-
+    this.hand.reduce(function(a, b) {
+        return a + b;
+    });
 };
 
 Player.prototype.toString = function() {
-
+    let output = this.name + ' ';
+    this.hand.forEach(function(card) {
+        output += card.toString() + ' ';
+    });
+    return output;
 };
 
 Player.prototype.requestCard = function() {
-
+    if (this.inPlay) {
+        return this.valueOf() < 15;
+    }
+    return false;
 };
+
+Player.prototype.addToHand = function(card) {
+    let theHand = this.hand;
+    if (Card.isValid(card)) {
+        theHand.push(card.clone());
+    }
+    this.hand = theHand;
+};
+
+Player.prototype.reset = function() {
+    this.hand = [];
+};
+
+/*
+* Exports.
+*/
+module.exports = Player;
 
 
