d1efad607b7f073e49f689f7ae0678bef8de44f4
Molly Arhammar <mollyarhammar@gmail.com>
2016-10-12 22:24:33 +0100

    adds a Board type to keep track of the round mechanichs

---
+++ src/Board.js
@@ -0,0 +1,68 @@
+/**
+ * Module for Board.
+ *
+ * @author Molly Arhammar
+ * @version 1.0.0
+ */
+
+'use strict';
+
+let Player = require('../src/Player.js');
+let Dealer = require('../src/Dealer.js');
+let Deck = require('../src/Deck.js');
+
+function Board() {
+    let theDeck = new Deck();
+    let theDealer = new Dealer();
+
+    Object.defineProperties(this, {
+        theDealer: {
+            get: function() {
+                return theDealer;
+            },
+            enumerable: true,
+            configurable: false
+        },
+        theDeck: {
+            get: function() {
+                return theDeck;
+            },
+            enumerable: true,
+            configurable: false
+        }
+    });
+}
+
+Board.prototype.round = function(thePlayer) {
+    let theWinner;
+    this.theDeck.shuffle();
+
+    while (thePlayer.requestCard) {
+        let theCard = this.theDeck.deal();
+        thePlayer.addToHand(theCard);
+        if ((thePlayer.points === 21) || (thePlayer.hand.length === 4 && thePlayer.points < 21)) {
+            theWinner = thePlayer;
+            break;
+        } else if (thePlayer.points > 21) {
+            theWinner = this.theDealer;
+            break;
+        }
+    }
+
+    if (!theWinner) {
+        while (this.theDealer.requestCard) {
+            this.theDealer.addToHand(this.theDeck.deal());
+            if (this.theDealer.points > thePlayer.points) {
+                theWinner = this.theDealer;
+                break;
+            } else if (this.theDealer.points > 21) {
+                theWinner = thePlayer;
+                break;
+            }
+        }
+    }
+
+    return theWinner;
+};
+
+module.exports = Board;
