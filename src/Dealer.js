ef38c6f8742eb878fb6868ddc859bb5c167d8a2c
Molly Arhammar <mollyarhammar@gmail.com>
2016-10-12 21:18:16 +0100

    adds a dealer type that inherits from player

---
+++ src/Dealer.js
@@ -0,0 +1,23 @@
+/**
+ * Module for Dealer.
+ * @augments Player
+ *
+ * @author Molly Arhammar
+ * @version 1.0.0
+ */
+
+'use strict';
+
+const Card = require('../src/Card.js');
+const Player = require('../src/Player.js');
+
+function Dealer(name) {
+    Player.call(this, name);
+}
+
+Dealer.prototype = Object.create(Player.prototype);
+Dealer.prototype.constructor = Dealer;
+
+Dealer.prototype.requestCard = function() {
+    return this.inPlay();
+};
