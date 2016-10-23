0648726d6f965c43675b6c4d6b61c2b14900f0e3
Molly Arhammar <mollyarhammar@gmail.com>
2016-10-23 14:25:52 +0100

    adds CardPlayer tests

--- test/CardPlayer.test.js
+++ test/CardPlayer.test.js
@@ -1,3 +1,31 @@
 /**
- * Created by Molly on 2016-10-21.
+ * Tests for the CardPlayer type.
+ *
+ * @author Molly Arhammar
+ * @version 1.16.1
  */
+
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('CardPlayer', () => {
+
+  let CardPlayer;
+
+  describe('Type', () => {
+    it('should be defined', (done) => {
+      CardPlayer = require('../src/CardPlayer.js');
+      done();
+    });
+  });
+
+  describe('Constructor', () => {
+    it('should be abstract', (done) => {
+      expect(() => {
+        new CardPlayer();
+      }).to.throw(Error);
+        done();
+    });
+  });
+});
