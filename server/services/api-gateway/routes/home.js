b6147e0edeb95042a3660448f2255d4d49b24382
Molly Arhammar <mollyarhammar@gmail.com>
2018-02-07 10:06:51 +0100

    use jwt to communicate with github service

--- server/services/api-gateway/routes/home.js
+++ server/services/api-gateway/routes/home.js
@@ -5,7 +5,8 @@
 // Requires.
 let router = require('express').Router()
 let passport = require('passport')
-let jwt = require('../resources/auth/jwt')
+let jwtcreator = require('../resources/auth/jwt')
+let axios = require('axios')
 
 // Routes.
 router.route('/')
@@ -17,7 +18,7 @@ router.route('/')
  * Authenticate the user through github, request scopes.
  */
 router.route('/login')
-    .get(passport.authenticate('github', {scope: ['user']}))
+    .get(passport.authenticate('github', {scope: ['user', 'repo', 'admin:org_hook']}))
 
 /**
  * Handle the authentication.
@@ -28,8 +29,23 @@ router.route('/login/success')
         if (err) {
           return next(err)
         } else {
-          res.set('Authorization', 'Bearer ' + jwt.create({user: user.username}))
-          return res.json(user)
+          let jwt = jwtcreator.create({user: user.username})
+
+          axios({
+            method: 'put',
+            headers: {'Authorization': 'Bearer ' + jwt},
+            url: process.env.GITHUB_SERVICE + '/user',
+            data: {
+              accessToken: user.accessToken
+            }
+          })
+          .then((response) => {
+            res.set('Authorization', 'Bearer ' + jwt)
+            return res.json(user)
+          })
+          .catch((err) => {
+            return next({message: err})
+          })
         }
       })(req, res, next)
     })
