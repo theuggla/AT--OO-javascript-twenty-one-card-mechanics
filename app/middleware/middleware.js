dee90db0ada2bc7385399ccbcc9ff423d88be2cc
Molly Arhammar <mollyarhammar@gmail.com>
2018-02-22 10:12:50 +0100

    update auth for trip passengers

--- app/middleware/middleware.js
+++ app/middleware/middleware.js
@@ -1,4 +1,5 @@
 let errs = require('restify-errors')
+let Trip = require('../models/PlannedTrip')
 
 module.exports.acceptJSON = function (req, res, next) {
   if (req.accepts('application/json')) {
@@ -17,6 +18,19 @@ module.exports.getAuthLevel = function (req, res, next) {
   }
 
   return next()
+}
+
+module.exports.getAuthLevelForTrip = function (req, res, next) {
+  Trip.findOne({_id: req.params.id})
+  .then((trip) => {
+    if (req.user.id === trip._creator || (trip.passengers.indexOf(req.user.id) > -1)) {
+      req.user.authorized = true
+    } else {
+      req.user.authorized = false
+    }
+
+    return next()
+  })
 }
 
 module.exports.authorize = function (req, res, next) {
